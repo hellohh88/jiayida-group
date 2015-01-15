5dd2490c1ec1bd4450bf8217112137e40f539316
hellohh88 <wangwenying6316@163.com>
2015-01-16 00:12:53 +0800

    alipay add initial source

---
+++ jiayida-mobile/src/main/java/com/joinway/mobile/alipay/util/httpClient/HttpProtocolHandler.java
@@ -0,0 +1,144 @@
+package com.joinway.mobile.alipay.util.httpClient;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+/* *
+ *类名：HttpProtocolHandler
+ *功能：HttpClient方式访问
+ *详细：获取远程HTTP数据
+ *版本：3.3
+ *日期：2012-08-17
+ *说明：
+ *以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
+ *该代码仅供学习和研究支付宝接口使用，只是提供一个参考。
+ */
+
+public class HttpProtocolHandler {
+
+    private static String              DEFAULT_CHARSET                     = "GBK";
+
+    /** 连接超时时间，由bean factory设置，缺省为8秒钟 */
+    private int                        defaultConnectionTimeout            = 8000;
+
+    /** 回应超时时间, 由bean factory设置，缺省为30秒钟 */
+    private int                        defaultSoTimeout                    = 30000;
+
+    /** 闲置连接超时时间, 由bean factory设置，缺省为60秒钟 */
+    private int                        defaultIdleConnTimeout              = 60000;
+
+    private int                        defaultMaxConnPerHost               = 30;
+
+    private int                        defaultMaxTotalConn                 = 80;
+
+    /** 默认等待HttpConnectionManager返回连接超时（只有在达到最大连接数时起作用）：1秒*/
+    private static final long          defaultHttpConnectionManagerTimeout = 3 * 1000;
+
+    /**
+     * HTTP连接管理器，该连接管理器必须是线程安全的.
+     */
+    private PoolingHttpClientConnectionManager      connectionManager;
+
+    private static HttpProtocolHandler httpProtocolHandler                 = new HttpProtocolHandler();
+
+    /**
+     * 工厂方法
+     * 
+     * @return
+     */
+    public static HttpProtocolHandler getInstance() {
+        return httpProtocolHandler;
+    }
+
+    /**
+     * 私有的构造方法
+     */
+    private HttpProtocolHandler() {
+        // 创建一个线程安全的HTTP连接池
+        connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultMaxPerRoute(defaultMaxConnPerHost);
+        connectionManager.setMaxTotal(defaultMaxTotalConn);
+    }
+
+    /**
+     * 执行Http请求
+     * 
+     * @param request 请求数据
+     * @param strParaFileName 文件类型的参数名
+     * @param strFilePath 文件路径
+     * @return 
+     * @throws HttpException, IOException 
+     */
+    public HttpResponse execute(HttpRequest request, String strParaFileName, String strFilePath) throws IOException {
+    	CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        // 设置连接超时
+        int connectionTimeout = defaultConnectionTimeout;
+        if (request.getConnectionTimeout() > 0) {
+            connectionTimeout = request.getConnectionTimeout();
+        }
+        // 设置回应超时
+        int soTimeout = defaultSoTimeout;
+        if (request.getTimeout() > 0) {
+            soTimeout = request.getTimeout();
+        }
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(soTimeout).setConnectTimeout(connectionTimeout).build();//设置请求和传输超时时间
+        
+        String charset = request.getCharset();
+        charset = charset == null ? DEFAULT_CHARSET : charset;
+
+        HttpEntity entity = null;
+        HttpResponse response = new HttpResponse();
+        //get模式且不带上传文件
+        if (request.getMethod().equals(HttpRequest.METHOD_GET)) {
+            HttpGet httpGet=new HttpGet(request.getUrl());
+            httpGet.setConfig(requestConfig);
+
+        	CloseableHttpResponse closeableResponse = httpClient.execute(httpGet);//执行请求
+        	entity = closeableResponse.getEntity();
+        	
+        } else if(strParaFileName.equals("") && strFilePath.equals("")) {
+        }
+        else {
+        }
+        response.setStringResult(EntityUtils.toString(entity));
+        return response;
+    }
+
+    /**
+     * 将NameValuePairs数组转变为字符串
+     * 
+     * @param nameValues
+     * @return
+     */
+    protected String toString(NameValuePair[] nameValues) {
+        if (nameValues == null || nameValues.length == 0) {
+            return "null";
+        }
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < nameValues.length; i++) {
+            NameValuePair nameValue = nameValues[i];
+
+            if (i == 0) {
+                buffer.append(nameValue.getName() + "=" + nameValue.getValue());
+            } else {
+                buffer.append("&" + nameValue.getName() + "=" + nameValue.getValue());
+            }
+        }
+
+        return buffer.toString();
+    }
+}
